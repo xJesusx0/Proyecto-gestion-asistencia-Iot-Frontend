d3959d2cdb556b4d08edba29ae0ed2377de54ee5
jesus <jesuspl0411@gmail.com>
2024-07-20 12:11:07 -0500

    archivo de las vista de profesor

--- js/teachers/teachers.js
+++ js/teachers/teachers.js
@@ -3,13 +3,55 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(userData)
     document.getElementById('welcomeUser').innerHTML += ` ${userData['names']} 🌟`
 
-    request('GET',url)
+    request('GET', url)
     .then(response => {
         console.log(response)
+        const cardsContainer = document.getElementById('cards-container');
 
         response.forEach(group => {
-            console.log(group)
-        })
-        
+            const card = document.createElement('div');
+            card.classList.add('card', 'mb-3');
+
+            const cardHeader = document.createElement('div');
+            cardHeader.classList.add('card-header');
+            card.appendChild(cardHeader);
+
+            const cardTitle = document.createElement('h5');
+            cardTitle.classList.add('card-title');
+            cardTitle.textContent = 'Información del grupo';
+            cardHeader.appendChild(cardTitle);
+
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+
+            const infoContainer = document.createElement('ul');
+            infoContainer.classList.add('list-unstyled');
+
+            const details = [
+                { label: 'Nombre', value: group.nombre },
+                { label: 'ID del grupo', value: group.id_grupo },
+                { label: 'Día de la semana', value: group.dia_semana },
+                { label: 'Período', value: group.periodo }
+            ];
+
+            details.forEach(detail => {
+                const listItem = document.createElement('li');
+                listItem.innerHTML = `<strong>${detail.label}:</strong> ${detail.value}`;
+                infoContainer.appendChild(listItem);
+            });
+
+            cardBody.appendChild(infoContainer);
+
+            const button = document.createElement('a');
+            button.href = `group-info.html?group_id=${group.id_grupo},module_id=${group.id_modulo},period=${group.periodo}`;
+            button.classList.add('btn', 'btn-primary');
+            button.textContent = 'Ver más información del grupo';
+            cardBody.appendChild(button);
+
+            card.appendChild(cardBody);
+
+            cardsContainer.appendChild(card);
+        });
+
     })
 })
