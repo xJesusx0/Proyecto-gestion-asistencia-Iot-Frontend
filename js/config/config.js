9b79d2ccf713a886693af761699b43146b1648a9
jesus <jesuspl0411@gmail.com>
2024-07-20 08:58:57 -0500

    prueba despliegue en netlify

--- js/config/config.js
+++ js/config/config.js
@@ -1,7 +1,9 @@
 const config = {
     serverIp: '192.168.1.8',  // Poner tu IP
     serverPort: '4000',
-    SECRET_TOKEN: '1234'
+    SECRET_TOKEN: '418a5069c96fffdcf3d8c60736eb50ef5223d64ae16918010088d883168fbe5f'
 };
 
-config.baseUrl = `http://${config.serverIp}:${config.serverPort}`;
+//config.baseUrl = `http://${config.serverIp}:${config.serverPort}`;
+config.baseUrl = 'https://proyecto-gestion-asistencia-iot-backend.vercel.app'
+//https://proyecto-gestion-asistencia-iot-backend.vercel.app/auth/validate-login
