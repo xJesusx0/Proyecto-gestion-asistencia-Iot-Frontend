4361dbe0a668c9da71468a86104cb5291e3c8a40
AlbertoP18 <alberto.perez9@misena.edu.co>
2024-07-20 13:36:13 -0500

    interfaz de justificacion

--- js/config/config.js
+++ js/config/config.js
@@ -1,9 +1,9 @@
 const config = {
     serverIp: '192.168.1.13',  // Poner tu IP
     serverPort: '4000',
-    SECRET_TOKEN: '418a5069c96fffdcf3d8c60736eb50ef5223d64ae16918010088d883168fbe5f'
+    SECRET_TOKEN: '1234'
 };
 
-//config.baseUrl = `http://${config.serverIp}:${config.serverPort}`;
-config.baseUrl = 'https://proyecto-gestion-asistencia-iot-backend.vercel.app'
+config.baseUrl = `http://${config.serverIp}:${config.serverPort}`;
+//config.baseUrl = 'https://proyecto-gestion-asistencia-iot-backend.vercel.app'
 //https://proyecto-gestion-asistencia-iot-backend.vercel.app/auth/validate-login
