6d997a0c3590712340b5ef169d91ac23821aeb30
jesus <jesuspl0411@gmail.com>
2024-07-21 12:31:51 -0500

    conexion con la api

--- js/config/config.js
+++ js/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     serverIp: 'https://proyecto-gestion-asistencia-iot-backend.vercel.app',  // Poner tu IP
-    serverPort: '4000',
+    serverPort: '',
     SECRET_TOKEN: '418a5069c96fffdcf3d8c60736eb50ef5223d64ae16918010088d883168fbe5f'
 };
 
