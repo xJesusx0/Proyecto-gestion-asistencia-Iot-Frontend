68a4e886bb6d1a4467b55c92b3d1179b0f565e75
jesus <jesuspl0411@gmail.com>
2024-07-22 13:36:26 -0500

    conexion con la api

--- js/config/config.js
+++ js/config/config.js
@@ -4,6 +4,6 @@ const config = {
     SECRET_TOKEN: '418a5069c96fffdcf3d8c60736eb50ef5223d64ae16918010088d883168fbe5f'
 };
 
-config.baseUrl = `http://${config.serverIp}${config.serverPort}`;
-//config.baseUrl = 'https://proyecto-gestion-asistencia-iot-backend.vercel.app'
+//config.baseUrl = `http://${config.serverIp}:${config.serverPort}`;
+config.baseUrl = 'https://proyecto-gestion-asistencia-iot-backend.vercel.app'
 //https://proyecto-gestion-asistencia-iot-backend.vercel.app/auth/validate-login
