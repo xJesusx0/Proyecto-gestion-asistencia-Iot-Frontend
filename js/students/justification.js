d96d05057e515703c4a01fa858a46ebf3994fecd
jesus <jesuspl0411@gmail.com>
2024-07-31 15:29:00 -0500

    se envia la informacion de la inasistencia y un mensaje

--- js/students/justification.js
+++ js/students/justification.js
@@ -1,11 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    getFails()
+})
+
+const sendJustification = () => {
+    const failsContainer = document.getElementById('fails-container');
+    const selectedValue = failsContainer.value;
+    console.log(selectedValue)
+}
+
+const getFails = () => {
+    const url = `${config.baseUrl}/students/get-student-fails`;
+
+    request('GET',url)
+    .then(response => {
+        console.log(response)
+        const failsContainer = document.getElementById('fails-container');
+        response.forEach(fail => {
+            const option = document.createElement('option');
+            const uniqueValue = `${fail.id_estudiante},${fail.id_grupo},${fail.id_modulo},${fail.periodo},${fail.fecha}`;
+            option.setAttribute('value', uniqueValue);
+
+            option.textContent = `${fail.nombre} (${fail.fecha})`;
+            
+            failsContainer.appendChild(option);
+        })
+
+    })
+}
 
 const sendFile = () => {
     const url = `${config.baseUrl}/students/set-justification`
     const formData = new FormData();
     const fileField = document.getElementById('file');
 
-    formData.append('file', fileField.files[0]);
+    const failsContainer = document.getElementById('fails-container');
+    const selectedValue = failsContainer.value;
 
+    const textareaField = document.getElementById('message');
+    
+    formData.append('file', fileField.files[0]);
+    formData.append('fail-id',selectedValue);
+    formData.append('message',textareaField.value)
+    
     const token = localStorage.getItem('access_token')
 
     fetch(url, {
