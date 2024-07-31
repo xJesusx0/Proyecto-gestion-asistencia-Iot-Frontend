990f1035197b69c4b70546e4478fcad5ca840de5
jesus <jesuspl0411@gmail.com>
2024-07-30 21:14:48 -0500

    conexion de los archivos con la api

--- js/students/justification.js
+++ js/students/justification.js
@@ -1,67 +1,31 @@
-fetch(window.jsonRoutes.studentsData)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('No se pudo leer el archivo JSON')
+
+const sendFile = () => {
+    const url = `${config.baseUrl}/students/set-justification`
+    const formData = new FormData();
+    const fileField = document.getElementById('file');
+
+    formData.append('file', fileField.files[0]);
+
+    const token = localStorage.getItem('access_token')
+
+    fetch(url, {
+        headers: {
+        'Authorization': `Bearer ${token}`
+    },
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data['response']){
+            alert('Archivo subido correctamente')
+            return;
         }
 
-        return response.json();
+        alert(data['error'])
+        console.log(data)
     })
-
-    .then(data => {
-
-        data = data['0']
-
-        const userId = userData.userId;
-        console.log(userData.username)
-
-        console.log(userId);
-        console.log(data);
-        student = data[userId];
-
-        console.log(student['name'])
-        document.getElementById('btnDesplegable').innerHTML = student['name']
-        document.getElementById('program-container').innerHTML = `Programa: ${student['program']}`
-        document.getElementById('period-container').innerHTML = `Cuatrimestre: ${student['period']} `
-
-        fetch(window.jsonRoutes.failsData)
-            .then(response => {
-                return response.json();
-            })
-
-            .then(failsData => {
-
-                const studentFails = failsData[student.id];
-                
-                const select = document.getElementById('inasistencia');
-                studentFails.forEach(fail => {
-                    const optionElement = document.createElement('option');
-                    optionElement.value = fail.group;
-                    optionElement.textContent = `${fail.course}, ${fail.date}`;
-                    select.appendChild(optionElement);
-                });
-            })
-
-
-
-    })
-
-// const opciones = [
-//     { valor: 'matematicas', texto: 'Paginas web' },
-//     { valor: 'ciencias', texto: 'Programacion modular' },
-//     { valor: 'historia', texto: 'Proyecto integrador' },
-//     { valor: 'lengua', texto: 'Competencias Digitales' }
-// ];
-
-// // Obtener el elemento select
-// const select = document.getElementById('inasistencia');
-
-// // Generar opciones dinámicamente
-// opciones.forEach(opcion => {
-//     const optionElement = document.createElement('option');
-//     optionElement.value = opcion.valor;
-//     optionElement.textContent = opcion.texto;
-//     select.appendChild(optionElement);
-// });
-
-
-
+    .catch(error => {
+        alert(error.error);
+    });
+}
