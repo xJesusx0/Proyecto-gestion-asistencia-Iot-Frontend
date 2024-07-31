4ef4f47a3a928e8997f43dc27c6a5f1f3a22d044
jesus <jesuspl0411@gmail.com>
2024-07-31 18:04:30 -0500

    cambios menores

--- js/request.js
+++ js/request.js
@@ -21,7 +21,7 @@ const request = async (method, url, data = null) => {
         return response.data;
     } catch (error) {
         if (error.response) {
-            alert(error.response.data.error)
+        //    alert(error.response.data.error)
             console.error(error.response.data)
             throw new Error(`Error ${error.response.status}`);
         } else if (error.request) {
