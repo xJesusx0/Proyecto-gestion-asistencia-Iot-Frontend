e87af3283e091cebe953fff890951826e5c9d7d1
jesus <jesuspl0411@gmail.com>
2024-08-03 14:21:12 -0500

    descargar justificante funciona correctamente

--- js/teachers/fails.js
+++ js/teachers/fails.js
@@ -48,7 +48,7 @@ const getFails = (groupId, moduleId, period) => {
                         <p class="card-text"><strong>Aprobada:</strong> ${data.aprobada === 0 ? 'no' : 'sí'}</p>
                         <p class="card-text"><strong>Período:</strong> ${data.periodo}</p>
                         <button class="btn btn-success" onclick="approveJustification(${data.id_estudiante}, '${data.id_grupo}', '${data.id_modulo}', '${data.periodo}', '${data.fecha}')">Aprobar justificación</button>
-                        <button class="btn btn-info">Ver Justificante</button>
+                        <button class="btn btn-info" onclick = "getJustificationUrl(${data.id_estudiante}, '${data.id_grupo}', '${data.id_modulo}', '${data.periodo}', '${data.fecha}')">Ver Justificante</button>
                     </div>
                 </div>
             `;
@@ -74,5 +74,61 @@ const approveJustification = (studentId,groupId,moduleId,period,date) => {
     request('POST',url,groupInfo)
     .then(response => {
         alert(response.response)
+        window.location.reload()
     })
 }
+const getJustificationUrl = (studentId,groupId,moduleId,period,date) => {
+
+    const url = `${config.baseUrl}/teachers/get-justification-url?student_id=${studentId}&group_id=${groupId}&module_id=${moduleId}&period=${period}&date=${date}`;
+
+    console.log(url)
+
+    request('GET',url)
+    .then(response => {
+        console.log(response)
+        
+        const path = response.url;
+        
+        const modifiedPath = path.slice(1);
+        console.log(modifiedPath)
+        
+        downloadFile(modifiedPath) 
+        //downloadFile('1235,SIN_G3,COM25,2024-2,2024-07-31.jpeg');
+    
+    })
+
+
+    //const url = `${config.baseUrl}/download/${filename}`;
+    //window.location.href = url;  // Redirige al usuario para descargar el archivo
+}
+
+const downloadFile = async (filename) => {
+    try {
+        const token = localStorage.getItem('access_token')
+        const url = `${config.baseUrl}/teachers/download/${filename}`;
+        
+        // Realiza la solicitud de descarga
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const blob = await response.blob();
+
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+};
