5f44390e27d31d8f738166ecb65ed22b66e4efdf
jesus <jesuspl0411@gmail.com>
2024-08-03 10:55:35 -0500

    ruta para aprobar una justificacion

--- js/teachers/fails.js
+++ js/teachers/fails.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Hola')
     const urlParams = getUrlParams();
     getFails(urlParams.group_id, urlParams.module_id, urlParams.period)
-    container.appendChild(card);
 });
 
 const getUrlParams = () => {
@@ -27,10 +26,9 @@ const getFails = (groupId, moduleId, period) => {
     const url = `${config.baseUrl}/teachers/get-fails-by-group?group_id=${groupId}&module_id=${moduleId}&period=${period}`;
     request('GET', url)
         .then(response => {
-            console.log(response)
+            console.log(response);
             const container = document.getElementById('card-container');
             response.forEach(data => {
-
                 const card = document.createElement('div');
                 card.className = 'col-md-4 mb-3';
 
@@ -46,16 +44,35 @@ const getFails = (groupId, moduleId, period) => {
                         <p class="card-text"><strong>ID Estudiante:</strong> ${data.id_estudiante}</p>
                         <p class="card-text"><strong>ID Grupo:</strong> ${data.id_grupo}</p>
                         <p class="card-text"><strong>ID Módulo:</strong> ${data.id_modulo}</p>
-                        <p class="card-text"><strong>justificada:</strong> ${data.justificada === 0 ? 'no' : 'sí'}</p>
-                        <p class="card-text"><strong>aprobada:</strong> ${data.aprobada === 0 ? 'no' : 'sí'}</p>
+                        <p class="card-text"><strong>Justificada:</strong> ${data.justificada === 0 ? 'no' : 'sí'}</p>
+                        <p class="card-text"><strong>Aprobada:</strong> ${data.aprobada === 0 ? 'no' : 'sí'}</p>
                         <p class="card-text"><strong>Período:</strong> ${data.periodo}</p>
-                        <button class="btn btn-success">Aprobar justificacion</button>
+                        <button class="btn btn-success" onclick="approveJustification(${data.id_estudiante}, '${data.id_grupo}', '${data.id_modulo}', '${data.periodo}', '${data.fecha}')">Aprobar justificación</button>
                         <button class="btn btn-info">Ver Justificante</button>
                     </div>
                 </div>
             `;
 
                 container.appendChild(card);
-            })
-        })
+            });
+        });
+}
+
+
+const approveJustification = (studentId,groupId,moduleId,period,date) => {
+    console.log(studentId,groupId,moduleId,period,date)
+    const url = `${config.baseUrl}/teachers/approve-justification`
+
+    const groupInfo = {
+        'student_id':studentId,
+        'group_id':groupId,
+        'module_id':moduleId,
+        'period':period,
+        'date':date
+    }
+
+    request('POST',url,groupInfo)
+    .then(response => {
+        alert(response.response)
+    })
 }
