a0b228994fbef65710013a31c3cb4f6b1d7029a6
jesus <jesuspl0411@gmail.com>
2024-08-03 15:08:31 -0500

    cambios menores

--- js/config/config.js
+++ js/config/config.js
@@ -1,10 +1,10 @@
 const config = {
-    serverIp: '192.168.1.8',  // Poner tu IP
+    serverIp: '192.168.1.9',  // Poner tu IP
     serverPort: '4000',
     SECRET_TOKEN: '1234'
 };
 
-//config.baseUrl = `http://${config.serverIp}:${config.serverPort}`;
-config.baseUrl = 'https://proyecto-gestion-asistencia-iot-backend.vercel.app'
+config.baseUrl = `http://${config.serverIp}:${config.serverPort}`;
+//config.baseUrl = 'https://proyecto-gestion-asistencia-iot-backend.vercel.app'
 //https://proyecto-gestion-asistencia-iot-backend.vercel.app/auth/validate-login
 //config.baseUrl = 'https://proyecto-gestion-asistencia-iot-backend-a36zbrk8j.vercel.app'
