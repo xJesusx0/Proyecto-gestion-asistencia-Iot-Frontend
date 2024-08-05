b6e8dc534dd2816c5f83b20bc2affa70149fcf70
jesus <jesuspl0411@gmail.com>
2024-08-05 17:11:57 -0500

    cambios menores

--- js/nav.js
+++ js/nav.js
@@ -57,7 +57,38 @@ document.addEventListener('DOMContentLoaded', () => {
         </div>
     </nav>
 `;
-    const studentNav = `<nav class="navbar bg-body-tertiary fixed-top">
+
+    const teacherNav = `<nav class="navbar bg-body-tertiary fixed-top">
+        <div class="container-fluid">
+            
+        <img src="../img/logoiubLetras.png"  style="width: 200px; height: 50px;">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <img src="../img/logoiubLetras.png"  style="width: 200px; height: 50px;">
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                            <a class="nav-link" href="teachers.html">
+                                <button class="btn btn-primary btn-lg px-1 py-2" style="width: 350px;" type="button">Inicio</button>
+                            </a>
+                        </li>
+<li class="nav-item">
+                           
+                </div>
+            </div>
+        </div>
+    </nav>
+`
+
+    const studentNav = `<nav class="navbar bg-body-tertiary fixed-top mb-5">
         <div class="container-fluid">
             
         <img src="../img/logoiubLetras.png"  style="width: 200px; height: 50px;">
@@ -98,7 +129,8 @@ document.addEventListener('DOMContentLoaded', () => {
 `
     const navs = {
         'administrador': adminNav,
-        'estudiante': studentNav
+        'estudiante': studentNav,
+        'profesor':teacherNav
     }
     const navContainer = document.getElementById('nav-container');
     fetch('../json/web-routes.json')
