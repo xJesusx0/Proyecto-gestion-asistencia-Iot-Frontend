2b00b736fabca41f2bb5e6d16b5a090a773acb2a
jesus <jesuspl0411@gmail.com>
2024-08-06 18:37:19 -0500

    graficos nuevos

--- js/chart.js
+++ js/chart.js
@@ -1,8 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
     usersRolesChart();
     weekdayAttendances();
-    studentsByGroup()
+    studentsByGroup();
+    countGroupsByModule();
+    countFailsByModule();
+    countFails();
 });
+const getRandomColorWithTransparency = (alpha = 0.2) => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+//        "get-count-groups-by-module",
+//        "get-count-fails-by-module",
+//        "count-justificated-and-no-justifiacted-fails"
+const countFails = () => {
+    const url = `${config.baseUrl}/admin/count-justificated-and-no-justifiacted-fails`;
+
+    request('GET', url)
+        .then(jsonData => {
+            console.log(jsonData)
+            console.log(jsonData[0])
+            console.log(jsonData[0]['inasistencias_justificadas'])
+            const labels = ['Justificadas', 'No justificadas']
+            values = [jsonData[0]['inasistencias_justificadas'], jsonData[0]['inasistencias_no_justificadas']]
+            const backgrounds = [getRandomColorWithTransparency(), getRandomColorWithTransparency()];
+
+            circleChart(labels, values, 'count-fails', backgrounds, 'Total de inasistencias');
+        })
+}
+const countFailsByModule = () => {
+    const url = `${config.baseUrl}/admin/get-count-fails-by-module`;
+
+    request('GET', url)
+        .then(jsonData => {
+            console.log(jsonData)
+            const labels = jsonData.map(item => item.modulo);
+            const values = jsonData.map(item => item.total_inasistencias);
+
+            const backgrounds = [getRandomColorWithTransparency(), getRandomColorWithTransparency()];
+
+            barChart(labels, values, 'fails-by-group', backgrounds, 'Total de inasistencias');
+        })
+}
+const countGroupsByModule = () => {
+    const url = `${config.baseUrl}/admin/get-count-groups-by-module`;
+
+    request('GET', url)
+        .then(jsonData => {
+            console.log(jsonData)
+            const labels = jsonData.map(item => item.modulo);
+            const values = jsonData.map(item => item.total_grupos);
+
+            const backgrounds = [getRandomColorWithTransparency(), getRandomColorWithTransparency()];
+
+            barChart(labels, values, 'groups-by-module', backgrounds, 'Total de Grupos');
+        })
+}
+
 const studentsByGroup = () => {
     const url = `${config.baseUrl}/admin/count-students-by-group`;
     request('GET', url)
@@ -22,13 +78,12 @@ const studentsByGroup = () => {
                 values,
                 'students-group',
                 [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    getRandomColorWithTransparency(),
+                    getRandomColorWithTransparency(),
+                    getRandomColorWithTransparency(),
+                    getRandomColorWithTransparency()
                 ],
+                'Cantidad de estudiantes'
 
             )
         })
@@ -54,29 +109,45 @@ const weekdayAttendances = () => {
                 values,
                 'weekday-attendances',
                 [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    getRandomColorWithTransparency(),
+                    getRandomColorWithTransparency(),
+                    getRandomColorWithTransparency()
                 ],
-
+                'Asistencias por dia de semana'
             )
 
-            barChart(['cantidad total de asistencias'],
-                [totalAttendances],
-                'total-attendances',
-                'rgba(153, 102, 255, 0.2)'
-            )
         })
 }
 
-const barChart = (labels, values, id, backgrounds) => {
+const usersRolesChart = () => {
+    const url = `${config.baseUrl}/admin/get-count-roles`;
+    console.log(url)
+    request('GET', url)
+        .then(response => {
+            console.log(response)
+
+            const estudiantes = response.estudiantes;
+            const profesores = response.profesores;
+            const administradores = response.administradores;
+
+            const labels = ['Estudiantes', 'Profesores', 'Administradores'];
+            const values = [estudiantes, profesores, administradores]
+
+            const backgrounds = [
+                getRandomColorWithTransparency(),
+                getRandomColorWithTransparency(),
+                getRandomColorWithTransparency(),
+            ]
+
+            circleChart(labels, values, 'users-roles', backgrounds, 'Cantidad de usuarios por rol')
+        });
+}
+
+const barChart = (labels, values, id, backgrounds, label) => {
     const data = {
         labels: labels,
         datasets: [{
-            label: 'Asistencias',
+            label: label,
             data: values,
             backgroundColor: backgrounds,
             borderColor: 'rgba(255, 255, 255, 1)',
@@ -92,6 +163,15 @@ const barChart = (labels, values, id, backgrounds) => {
                 y: {
                     beginAtZero: true
                 }
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text:label 
+                }
             }
         }
     };
@@ -99,33 +179,12 @@ const barChart = (labels, values, id, backgrounds) => {
     new Chart(ctx, config);
 };
 
-const usersRolesChart = () => {
-    const url = `${config.baseUrl}/admin/get-count-roles`;
-    console.log(url)
-    request('GET', url)
-        .then(response => {
-            console.log(response)
-
-            const estudiantes = response.estudiantes;
-            const profesores = response.profesores;
-            const administradores = response.administradores;
-            const total = response.usuarios;
-
-            const labels = ['Estudiantes', 'Profesores', 'Administradores'];
-            const values = [estudiantes, profesores, administradores]
-
-            document.getElementById('total-users').innerHTML += total
-
-            circleChart(labels, values)
-        });
-}
-
-const circleChart = (labels, values) => {
+const circleChart = (labels, values, id, backgrounds, text) => {
     const data = {
         labels: labels,
         datasets: [{
-            label: 'Cantidad de usuarios por rol',
-            backgroundColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 99, 132, 1)', 'rgba(75, 192, 192,1)'],
+            label: text,
+            backgroundColor: backgrounds,
             borderColor: 'white',
             borderWidth: 0.5,
             data: values
@@ -143,12 +202,12 @@ const circleChart = (labels, values) => {
                 },
                 title: {
                     display: true,
-                    text: 'Cantidad de usuarios por rol'
+                    text: text
                 }
             }
         },
     };
 
-    var ctx = document.getElementById('users-roles').getContext('2d');
+    var ctx = document.getElementById(id).getContext('2d');
     var myChart = new Chart(ctx, chartConfig);
 }
